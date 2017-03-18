bc9fb3325b734fb765887f8814a48a1a2cd7a772
Avinash Singh <avinash4507@gmail.com>
2017-03-18 17:21:42 +0530

    Check

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
           Navigation: path.resolve(__dirname, "app/components/Nav.jsx"), 
           applicationStyles: path.resolve(__dirname, "app/styles/app.scss"),
         },
-        extensions: ['.js', '.jsx']
+        extensions: ['.js', '.jsx', '.scss']
     },
     module: {
         loaders: [
